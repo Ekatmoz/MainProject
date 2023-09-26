017bead166d2706fb3c4e08a080c3f1a67e1cfa6
Ekatmoz <ekatmoz@gmail.com>
2023-09-26 22:12:08 +0200

    navbar created and setup react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,18 @@
+import { ChakraProvider } from '@chakra-ui/react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div>
-      <h1>My online shop</h1>
+      <ChakraProvider>
+        <Router>
+          <Navbar/>
+          <main>
+            <Routes></Routes>
+          </main>
+        </Router>
+      </ChakraProvider>
     </div>
   );
 }
