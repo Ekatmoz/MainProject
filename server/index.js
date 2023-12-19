dd888f489af2e1a0463d94cbff8dcc18e013b84b
Ekatmoz <ekatmoz@gmail.com>
2023-12-19 13:56:30 +0100

    index.js update

--- server/index.js
+++ server/index.js
@@ -21,6 +21,7 @@ app.use('/api/users', userRoutes);
 app.use('/api/checkout', stripeRoute);
 app.use('/api/orders', orderRoutes);
 
+
 app.get('/api/config/GOOGLE', (req, res) =>
   res.send(process.env.GOOGLE_CLIENT_ID)
 );
@@ -35,15 +36,6 @@ if(process.env.NODE_ENV == 'production') {
 
   app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
 }
-
-app.use('/api/products', productRoutes);
-app.use('/api/users', userRoutes);
-app.use('/api/checkout', stripeRoute);
-app.use('/api/orders', orderRoutes);
-
-app.get('/api/config/GOOGLE', (req, res) =>
-  res.send(process.env.GOOGLE_CLIENT_ID)
-);
 
 app.listen(port, () => {
   console.log(`Server runs on port ${port}.`)
