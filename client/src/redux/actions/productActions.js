4b394e6d8a37d1e7ce528f75e9d627e890474689
Ekatmoz <ekatmoz@gmail.com>
2025-05-21 16:28:53 +0200

    debug

--- client/src/redux/actions/productActions.js
+++ client/src/redux/actions/productActions.js
@@ -16,7 +16,7 @@ export const getProducts = (page, category = null) => async (dispatch) => {
   dispatch(setLoading());
 
   try {
-    let url = `/api/products/${page}/${12}`; // 10 products per page
+    let url = `${import.meta.env.REACT_APP_API_URL}/api/products/${page}/${10}`; // 10 products per page
 
     // Add category parameter if specified
     if (category) {
@@ -103,7 +103,7 @@ export const toggleFavorites = (toggle) => async (dispatch, getState) => {
 export const getProduct = (id) => async (dispatch) => {
 	dispatch(setLoading(true));
 	try {
-		const { data } = await axios.get(`${import.meta.env.VITE_API_URL}/api/products/${id}`);
+		const { data } = await axios.get(`${import.meta.env.REACT_APP_API_URL}/api/products/${id}`);
 		dispatch(setProduct(data));
 	} catch (error) {
 		dispatch(
@@ -125,7 +125,7 @@ export const createProductReview = (productId, userId, comment, rating, title) =
 	try {
 		const config = { headers: { Authorization: `Bearer ${userInfo.token}`, 'Content-Type': 'application/json' } };
 
-		await axios.post(`${import.meta.env.VITE_API_URL}/api/products/reviews/${productId}`, { comment, userId, rating, title }, config);
+		await axios.post(`${import.meta.env.REACT_APP_API_URL}/api/products/reviews/${productId}`, { comment, userId, rating, title }, config);
 		dispatch(productReviewed(true));
 	} catch (error) {
 		dispatch(
